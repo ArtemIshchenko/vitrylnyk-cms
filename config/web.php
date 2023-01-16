fc69683438dc06a3f83d0a69fcb71fc725073b87
Artem <artem.ishchenko@skynix.co>
2023-01-16 15:28:18 +0200

    revision1

--- config/web.php
+++ config/web.php
@@ -19,7 +19,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'artemon11',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
